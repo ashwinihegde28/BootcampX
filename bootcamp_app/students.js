6c5adba934df51d6cdd2c05667fb9e1863afd659
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-04-05 18:14:23 -0400

    SQL Injection addressed

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,6 +7,39 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+/* Parameterized Query:
+ separate out our SQL into two different parts.
+The part that we write as the developer, the part that we have complete control over.
+The part that comes from somewhere else and might be malicious.*/
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+/*Each $s in our query is a placeholder that represents where a value should go but can't because it's coming from somewhere else, so it might be malicious.
+The $1 and $2 placeholders will eventually get replaced with the actual data from the values array. The numbering starts at 1 instead of 0, so the first value in the query $1 corresponds to the first value in the array values[0]*/
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+//run the query
+pool
+  .query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
+
+/* For reference
 //Accepting the inputs from the command line.
 //Where FEB is the cohort name and 2 is the maximum number of results
 pool
@@ -26,4 +59,4 @@ LIMIT ${process.argv[3] || 5};
       );
     });
   })
-  .catch((err) => console.error("query error", err.stack));
+  .catch((err) => console.error("query error", err.stack));*/
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,29 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+// parameterised query
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+  `;
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool
+  .query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
+
+/*
 //returns names of the teachers assisted, accepting the cohort name as input.
 pool
   .query(
@@ -25,3 +48,4 @@ ORDER BY teacher;
       console.log(`${row.cohort}: ${row.teacher}`);
     });
   });
+  */
