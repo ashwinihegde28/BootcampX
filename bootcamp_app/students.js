33d265b4d877bd1916f3a8f10eab42d610c5a091
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-04-05 16:58:08 -0400

    Queries with Node Postgres ans command line argument

---
+++ bootcamp_app/students.js
@@ -0,0 +1,29 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+//Accepting the inputs from the command line.
+//Where FEB is the cohort name and 2 is the maximum number of results
+pool
+  .query(
+    `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+//returns names of the teachers assisted, accepting the cohort name as input.
+pool
+  .query(
+    `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+ORDER BY teacher;
+`
+  )
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  });
